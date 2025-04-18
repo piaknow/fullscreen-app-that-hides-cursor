985958ab6a26d453c5a587ee8674123689da8ae7
piaknow <151747287+piaknow@users.noreply.github.com>
2025-04-18 22:58:00 +0900

    Add files via upload

---
+++ fullscreen app that hides cursor/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace fullscreen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FullScreen = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // FullScreen
+            // 
+            this.FullScreen.Location = new System.Drawing.Point(246, 167);
+            this.FullScreen.Name = "FullScreen";
+            this.FullScreen.Size = new System.Drawing.Size(75, 23);
+            this.FullScreen.TabIndex = 0;
+            this.FullScreen.Text = "FullScreen";
+            this.FullScreen.UseVisualStyleBackColor = true;
+            this.FullScreen.Click += new System.EventHandler(this.FullScreen_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.FullScreen);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown_1);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button FullScreen;
+    }
+}
+
---
+++ fullscreen app that hides cursor/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace fullscreen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private bool cursorstate = true;
+        private void FullScreen_Click(object sender, EventArgs e)
+        {
+            FormBorderStyle = FormBorderStyle.None;
+            WindowState = FormWindowState.Maximized;
+            TopMost = true;
+            if (cursorstate == true)
+            {
+                Cursor.Hide();
+                cursorstate = false;
+            }
+        }
+
+        private void Form1_KeyDown_1(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                FormBorderStyle = FormBorderStyle.Sizable;
+                WindowState = FormWindowState.Normal;
+                TopMost = false;
+                if (cursorstate == false)
+                {
+                    Cursor.Show();
+                    cursorstate = true;
+                }
+            }
+        }
+    }
+}
